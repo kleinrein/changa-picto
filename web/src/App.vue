48831a324e968e9482e3f7ba5a7aa61194f6668d
Andreas Rein <rein@live.no>
2017-03-10 18:26:47 +0100

    Init with vue2 template

---
+++ web/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="app">
+    <h4>changa picto</h4>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style src="./styles/main.scss" lang="scss"></style>
---
+++ web/src/components/Home.vue
@@ -0,0 +1,14 @@
+<template>
+<div class="hello">
+	<input type="text" placeholder="search" class="input-search" v-model="search" />
+</div>
+</template>
+
+<script>
+export default {
+	name: 'hello',
+	data() {
+		return {}
+	}
+}
+</script>
