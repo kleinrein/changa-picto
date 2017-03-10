4f691f032c95d19b0813502f2a07312b56a52a1d
Andreas Rein <rein@live.no>
2017-03-11 00:38:54 +0100

    Icon loading and hexagon

--- web/src/components/Home.vue
+++ web/src/components/Home.vue
@@ -1,14 +1,37 @@
 <template>
 <div class="hello">
 	<input type="text" placeholder="search" class="input-search" v-model="search" />
+
+	<div class="icons">
+		<div v-for="icon in icons" class="icon hexagon">
+			<span class="icon-title">{{icon.title}}</span>
+			<img class="icon-img" :src="imgUrl(icon.filename)">
+		</div>
+	</div>
 </div>
 </template>
 
 <script>
+var icons = require.context('../../../svg/', false, /\.svg$/)
+
 export default {
 	name: 'hello',
 	data() {
-		return {}
+		return {
+			imgUrl: function (path) {
+		      return icons('./' + path)
+		   	},
+			search: '',
+			icons: [{
+					title: 'Comment',
+					filename: "comment.svg"
+				},
+				{
+					title: 'Comment O',
+					filename: 'comment-o.svg'
+				}
+			]
+		}
 	}
 }
 </script>
