1cfeca5cc09b26e366203cf24ead4c0ce754cb71
Andreas Rein <rein@live.no>
2017-03-28 19:14:28 +0200

    Some nice styling

--- web/src/components/Home.vue
+++ web/src/components/Home.vue
@@ -2,8 +2,10 @@
 	<div class="hello">
 		<input type="text" placeholder="search" class="input-search" v-model="search" />
 
+		<modal v-if="showModal" @close="showModal = false" />
+
 		<div class="icons">
-			<div v-for="icon in icons" v-if="icon.replace('.svg', '') === search || icon.replace('.svg', '').includes(search)" class="icon">
+			<div @click="showModal = true" v-for="icon in icons" v-if="icon.replace('.svg', '') === search || icon.replace('.svg', '').includes(search)" class="icon">
 				<span class="icon-title">{{icon.replace('.svg', '')}}</span>
 				<img class="icon-img" :src="imgUrl(icon)">
 			</div>
@@ -18,6 +20,7 @@ export default {
 	name: 'hello',
 	data() {
 		return {
+			showModal: false,
 			imgUrl: function (path) {
 		      return icons('./' + path)
 		   	},
