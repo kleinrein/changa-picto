9934e4a920f7a2ea10658a5ef78e0b41bde745a2
Andreas Rein <rein@live.no>
2017-04-24 20:40:32 +0200

    Style fixes

--- web/src/javascripts/app.js
+++ web/src/javascripts/app.js
@@ -1,4 +1,5 @@
 import Icon from './modules/icon'
+import * as dynamics from 'dynamics.js'
 
 let icons = []
 let filteredIcons = []
@@ -11,21 +12,35 @@ Array.from(document.querySelectorAll(".icon")).forEach(el => {
 
     el.addEventListener('click', function(event) {
         icon.showModal(modal)
+
         icons.forEach(icon => icon.el.querySelector("img").style.filter = 'blur(5px)')
+        document.querySelector(".input-search").style.filter = 'blur(5px)'
     })
 })
 
+// Close modal
 const overlay = document.querySelector(".overlay")
 overlay.addEventListener("click", () => {
     overlay.style.display = 'none'
-    modal.style.display = 'none'
+
     icons.forEach(icon => icon.el.querySelector("img").style.filter = 'blur(0)')
+    document.querySelector(".input-search").style.filter = 'blur(0)'
+
+    dynamics.animate(modal, {
+        scale: 0,
+    }, {
+        type: dynamics.spring,
+        frequency: 200,
+        friction: 150,
+        duration: 400
+    })
 })
 
 // Search
 document.querySelector(".input-search").addEventListener('input', function(event) {
     const searchVal = event.target.value.toLowerCase()
-    if (searchVal === '') icons.forEach(icon => icon.setDisplay('flex'))
+    if (searchVal === '')
+        icons.forEach(icon => icon.setDisplay('flex'))
 
     const filteredIcons = icons.filter(icon => icon.title.toLowerCase().includes(searchVal))
     filteredIcons.forEach(icon => icon.setDisplay('flex'))
--- web/src/javascripts/modules/icon.js
+++ web/src/javascripts/modules/icon.js
@@ -1,16 +1,25 @@
+import * as dynamics from 'dynamics.js'
+
 export default class Icon {
     constructor(el, title) {
         this.el = el
-        this.title = this.el.querySelector(".icon-title")
-            .innerHTML
-            .replace("_", " ")
-            .replace(".svg", "")
+        this.title = this.el.querySelector(".icon-title").innerHTML.replace("_", " ").replace(".svg", "")
         this.path = this.el.querySelector("img").getAttribute("src")
     }
 
     showModal(modal) {
         console.log('show modal')
         modal.style.display = 'block'
+
+        dynamics.animate(modal, {
+            scale: 1.2
+        }, {
+            type: dynamics.spring,
+            frequency: 200,
+            friction: 550,
+            duration: 3500
+        })
+
         document.getElementById("modal-icon-title").innerHTML = this.title
         document.getElementById("modal-icon-download-svg").setAttribute("href", this.path)
         document.getElementById("modal-icon-img").setAttribute("src", this.path)
